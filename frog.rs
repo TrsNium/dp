f368a4411e39b604fdf9041dddab74ee35e4be54
TrsNium <trsnium@gmail.com>
2019-10-12 19:46:01 +0900

    frog https://atcoder.jp/contests/dp/tasks/dp_a

---
+++ frog.rs
@@ -0,0 +1,73 @@
+use std::cmp::max;
+use std::cmp::min;
+
+// ref. https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!{
+        n: usize,
+        height: [i32; n]
+    }
+
+    let mut dp: Vec<i32> = vec![1111111; n];
+    dp[0] = 0;
+
+    for n_ in 1..n{
+        dp[n_] = min(dp[n_], dp[n_-1]+(height[n_-1]-height[n_]).abs());
+        dp[n_] = if n_ > 1 {
+            min(dp[n_], dp[n_-2]+(height[n_-2]-height[n_]).abs())
+        }else{
+            dp[n_]
+        };
+    }
+
+    print!("{:?}", dp[n-1])
+}
