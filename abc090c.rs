1db34565308991ec3f68d32e846b52d2831bdf35
TrsNium <trsnium@gmail.com>
2019-10-15 23:10:41 +0900

    abc090c https://atcoder.jp/contests/arc090/tasks/arc090_a

---
+++ abc090c.rs
@@ -0,0 +1,69 @@
+use std::cmp::max;
+
+// ref. https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!{
+        n: usize,
+        v: [[usize;n];2]
+    }
+
+    let mut dp: Vec<Vec<usize>> = vec![vec![0;n+1];3];
+    for i in 0..2{
+        for n_ in 0..n{
+            dp[i][n_+1] = max(dp[i][n_], dp[i][n_]+v[i][n_]);
+            if i > 0{
+                dp[i][n_+1] = max(dp[i][n_+1], dp[0][n_+1]+v[i][n_] )
+            }
+        };
+    };
+    println!("{}", dp[1][n]);
+}
